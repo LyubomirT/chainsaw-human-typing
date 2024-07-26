9aa81edd6e599463e7c0ce27da7445fdaf2c651d
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-07-26 13:26:43 +0300

    chore: Update build script to include logo.png and style.css in the distribution folder

---
+++ build.ps1
@@ -0,0 +1,13 @@
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+pyinstaller --noconfirm --onedir --windowed --icon "$scriptDir\img.ico" --add-data "$scriptDir\logo.png;." --add-data "$scriptDir\style.css;."  "$scriptDir\main.py"
+
+# Copy the logo.png and style.css to the dist/main folder
+Copy-Item -Path "$scriptDir\logo.png" -Destination "$scriptDir\dist\main"
+Copy-Item -Path "$scriptDir\style.css" -Destination "$scriptDir\dist\main"
+
+# Delete the build folder
+Remove-Item -Path "$scriptDir\build" -Recurse
+
+# Delete the spec file
+Remove-Item -Path "$scriptDir\main.spec"
