392af088924fa925882446c0655b4f05bd1914b6
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-07-26 08:49:09 +0300

    Add text typing functionality to the application

--- main.py
+++ main.py
@@ -1,27 +1,82 @@
 import sys
 import time
 import pyautogui
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtCore import QTimer, Qt, QThread, pyqtSignal
 from ui import Ui_MainWindow
+
+class TypingThread(QThread):
+    progress = pyqtSignal(int)
+    finished = pyqtSignal()
+
+    def __init__(self, text, interval, type_enter, chars_per_stroke):
+        super().__init__()
+        self.text = text
+        self.interval = interval
+        self.type_enter = type_enter
+        self.chars_per_stroke = chars_per_stroke
+        self.running = True
+
+    def run(self):
+        i = 0
+        while i < len(self.text) and self.running:
+            if self.text[i] == '\n' and self.type_enter:
+                pyautogui.press('enter')
+                i += 1
+            else:
+                pyautogui.write(self.text[i:i + self.chars_per_stroke])
+                i += self.chars_per_stroke
+            time.sleep(self.interval)
+            self.progress.emit(int(i / len(self.text) * 100))
+        self.finished.emit()
+
+    def stop(self):
+        self.running = False
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
         self.startButton.clicked.connect(self.start_typing)
-    
+        self.stopButton.clicked.connect(self.stop_typing)
+        self.thread = None
+
     def start_typing(self):
         text = self.textEdit.toPlainText()
-        delay = int(self.delaySpinBox.value())
-        interval = float(self.intervalSpinBox.value())
-        
-        QTimer.singleShot(delay * 1000, lambda: self.type_text(text, interval))
-    
-    def type_text(self, text, interval):
-        for char in text:
-            pyautogui.typewrite(char)
-            time.sleep(interval)
+        try:
+            delay = int(self.delaySpinBox.value())
+            interval = float(self.intervalSpinBox.value())
+            type_enter = self.enterCheckBox.isChecked()
+            chars_per_stroke = int(self.charPerStrokeSpinBox.value())
+        except ValueError:
+            QMessageBox.critical(self, "Error", "Invalid input for delay, interval, or chars per stroke.")
+            return
+
+        self.startButton.setEnabled(False)
+        self.stopButton.setEnabled(True)
+        QTimer.singleShot(delay * 1000, lambda: self.initiate_typing(text, interval, type_enter, chars_per_stroke))
+
+    def initiate_typing(self, text, interval, type_enter, chars_per_stroke):
+        self.thread = TypingThread(text, interval, type_enter, chars_per_stroke)
+        self.thread.progress.connect(self.update_progress)
+        self.thread.finished.connect(self.typing_finished)
+        self.thread.start()
+
+    def update_progress(self, value):
+        self.progressBar.setValue(value)
+
+    def typing_finished(self):
+        self.startButton.setEnabled(True)
+        self.stopButton.setEnabled(False)
+        self.progressBar.setValue(0)
+
+    def stop_typing(self):
+        if self.thread:
+            self.thread.stop()
+            self.thread.wait()
+            self.thread = None
+        self.typing_finished()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- ui.py
+++ ui.py
@@ -3,36 +3,71 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400, 300)
+        MainWindow.resize(600, 400)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(20, 20, 360, 120))
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 20, 560, 340))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        
+        self.textEdit = QtWidgets.QTextEdit(self.horizontalLayoutWidget)
         self.textEdit.setObjectName("textEdit")
+        self.horizontalLayout.addWidget(self.textEdit)
         
-        self.delayLabel = QtWidgets.QLabel(self.centralwidget)
-        self.delayLabel.setGeometry(QtCore.QRect(20, 150, 60, 20))
+        self.settingsLayout = QtWidgets.QVBoxLayout()
+        self.settingsLayout.setObjectName("settingsLayout")
+        
+        self.delayLabel = QtWidgets.QLabel(self.horizontalLayoutWidget)
         self.delayLabel.setObjectName("delayLabel")
+        self.settingsLayout.addWidget(self.delayLabel)
         
-        self.delaySpinBox = QtWidgets.QSpinBox(self.centralwidget)
-        self.delaySpinBox.setGeometry(QtCore.QRect(90, 150, 60, 20))
+        self.delaySpinBox = QtWidgets.QSpinBox(self.horizontalLayoutWidget)
         self.delaySpinBox.setRange(0, 100)
         self.delaySpinBox.setObjectName("delaySpinBox")
+        self.settingsLayout.addWidget(self.delaySpinBox)
         
-        self.intervalLabel = QtWidgets.QLabel(self.centralwidget)
-        self.intervalLabel.setGeometry(QtCore.QRect(20, 180, 60, 20))
+        self.intervalLabel = QtWidgets.QLabel(self.horizontalLayoutWidget)
         self.intervalLabel.setObjectName("intervalLabel")
+        self.settingsLayout.addWidget(self.intervalLabel)
         
-        self.intervalSpinBox = QtWidgets.QDoubleSpinBox(self.centralwidget)
-        self.intervalSpinBox.setGeometry(QtCore.QRect(90, 180, 60, 20))
+        self.intervalSpinBox = QtWidgets.QDoubleSpinBox(self.horizontalLayoutWidget)
         self.intervalSpinBox.setRange(0.01, 10.0)
         self.intervalSpinBox.setSingleStep(0.01)
         self.intervalSpinBox.setObjectName("intervalSpinBox")
+        self.settingsLayout.addWidget(self.intervalSpinBox)
         
-        self.startButton = QtWidgets.QPushButton(self.centralwidget)
-        self.startButton.setGeometry(QtCore.QRect(20, 220, 360, 40))
+        self.charPerStrokeLabel = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.charPerStrokeLabel.setObjectName("charPerStrokeLabel")
+        self.settingsLayout.addWidget(self.charPerStrokeLabel)
+        
+        self.charPerStrokeSpinBox = QtWidgets.QSpinBox(self.horizontalLayoutWidget)
+        self.charPerStrokeSpinBox.setRange(1, 10)
+        self.charPerStrokeSpinBox.setObjectName("charPerStrokeSpinBox")
+        self.settingsLayout.addWidget(self.charPerStrokeSpinBox)
+        
+        self.enterCheckBox = QtWidgets.QCheckBox(self.horizontalLayoutWidget)
+        self.enterCheckBox.setObjectName("enterCheckBox")
+        self.settingsLayout.addWidget(self.enterCheckBox)
+        
+        self.startButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
         self.startButton.setObjectName("startButton")
+        self.settingsLayout.addWidget(self.startButton)
+        
+        self.stopButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.stopButton.setObjectName("stopButton")
+        self.stopButton.setEnabled(False)
+        self.settingsLayout.addWidget(self.stopButton)
+        
+        self.progressBar = QtWidgets.QProgressBar(self.horizontalLayoutWidget)
+        self.progressBar.setObjectName("progressBar")
+        self.settingsLayout.addWidget(self.progressBar)
+        
+        self.horizontalLayout.addLayout(self.settingsLayout)
         
         MainWindow.setCentralWidget(self.centralwidget)
         
@@ -44,5 +79,7 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "Text Typer"))
         self.delayLabel.setText(_translate("MainWindow", "Delay (s):"))
         self.intervalLabel.setText(_translate("MainWindow", "Interval (s):"))
+        self.charPerStrokeLabel.setText(_translate("MainWindow", "Chars per stroke:"))
+        self.enterCheckBox.setText(_translate("MainWindow", "Type Enter"))
         self.startButton.setText(_translate("MainWindow", "Start Typing"))
-
+        self.stopButton.setText(_translate("MainWindow", "Stop Typing"))
