9a858ed93dcc69a43a4c6b166c72333ac01b5177
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-07-30 06:12:55 +0300

    Move to src

--- build.ps1
+++ build.ps1
@@ -2,6 +2,8 @@ $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 pyinstaller --noconfirm --onedir --windowed --icon "$scriptDir\src\img.ico" --add-data "$scriptDir\src\logo.png;." --add-data "$scriptDir\src\style.css;."  "$scriptDir\src\main.py"
 
+
+
 # Copy the logo.png and style.css to the dist/main folder
 Copy-Item -Path "$scriptDir\src\logo.png" -Destination "$scriptDir\dist\main"
 Copy-Item -Path "$scriptDir\src\style.css" -Destination "$scriptDir\dist\main"
